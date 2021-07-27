7977ef23e07a168244ae6c42bcfc399c4387c57e
Julien David <david@lipn.univ-paris13.fr>
2021-07-27 15:41:07 +0200

    script variable added

--- plots/plot_algo_unique.sh
+++ plots/plot_algo_unique.sh
@@ -6,16 +6,18 @@ n=$2
 m=$3
 k=$4
 
+script=~/stage_entropy/plots/plotscript_temps_cmp.plot
+
 grep "$n $m" "$algorithm"_1000_100_2.txt > tmp/"$algorithm"_"$n"_"$m"_"$k".plot
 
 ## Lancement du script gnuplot
-sed -i 's/ALGORITHM_/'"${algorithm}"'_/g' ~/stage_entropy/plots/plotscript.plot
-sed -i 's/_N_/'"${n}"'/g' ~/stage_entropy/plots/plotscript.plot
-sed -i 's/_M_/'"${m}"'/g' ~/stage_entropy/plots/plotscript.plot
-cat ~/stage_entropy/plots/plotscript.plot | gnuplot 
-sed -i 's/'"${algorithm}"'_/ALGORITHM_/g' ~/stage_entropy/plots/plotscript.plot
-sed -i 's/'"${n}"'/_N_/g' ~/stage_entropy/plots/plotscript.plot
-sed -i 's/'"${m}"'/_M_/g' ~/stage_entropy/plots/plotscript.plot
+sed -i 's/ALGORITHM_/'"${algorithm}"'_/g' $script
+sed -i 's/_N_/'"${n}"'/g' $script
+sed -i 's/_M_/'"${m}"'/g' $script
+cat ~/stage_entropy/plots/plotscript_temps_cmp.plot | gnuplot 
+sed -i 's/'"${algorithm}"'_/ALGORITHM_/g' $script
+sed -i 's/'"${n}"'/_N_/g' $script
+sed -i 's/'"${m}"'/_M_/g' $script
 
 else
     echo "Veuillez entrer le nom de l'algorithme, n, m et k"
